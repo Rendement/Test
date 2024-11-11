c3295ec59874209701a92b9ab93b75e9b5d4ba9b
Ben Calvin <benjamincalvin@gmail.com>
2024-11-11 08:16:09 -0800

    added sass file

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f2683aaf66ba0d3de1f33165fc48ebb17b91cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6b02d14ba1a85a22b487a68bf0f3131bc29d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
